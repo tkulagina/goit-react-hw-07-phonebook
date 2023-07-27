6dd52c35175c9053c2ddc091bedfd7e923ae1a4f
tkulagina <tkulagina1974@gmail.com>
2023-07-27 11:05:27 +0300

    update store

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>      
-        <App />      
+<Provider store={store}>      
+         <App />     
     </Provider>
+    
   </React.StrictMode>
 );
+
